cfe54bc3feaa34c74c12567768bedd29a799af8f
Henk Daling <henkdaling2@gmail.com>
2023-11-16 17:58:14 -0600

    test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cmath>
 #include "SDL_Plotter.h"
+#include "../images/dots.c"
 
 using namespace std;
 
 void drawCircle(point loc, int size, color c, SDL_Plotter& g);
+void drawBmp(point loc,const img &im, SDL_Plotter& g);
 
 int main(int argc, char ** argv)
 {
@@ -27,19 +29,28 @@ int main(int argc, char ** argv)
 
 		if(g.mouseClick()){
 			p = g.getMouseClick();
-			size = rand()%50;
+			size = 10;
 			c.R  = rand()%256;
 			c.G  = rand()%256;
 			c.B  = rand()%256;
-			drawCircle(p, size, c, g);
+			//drawCircle(p, size, c, g);
+			drawBmp(p, gimp_image, g);
 		}
 		g.update();
 
     }
-
-	return 0;
 }
 
+void drawBmp(point loc,const img &im, SDL_Plotter& g){
+	
+	for(unsigned int i = 0; i < im.width; i++){
+		for(unsigned int j = 0; j < im.height; j = j + 3){
+			g.plotPixel((loc.x + double(i /10)) , (loc.y + double(j /10)) ,
+			 (color(int(im.pixel_data[i+j]), int(im.pixel_data[i + j + 1]), int(im.pixel_data[i + j + 2]))));
+
+		}
+	}
+}
 
 void drawCircle(point loc, int size, color c, SDL_Plotter& g){
 	for(double i = -size; i <= size;i+=0.1){
